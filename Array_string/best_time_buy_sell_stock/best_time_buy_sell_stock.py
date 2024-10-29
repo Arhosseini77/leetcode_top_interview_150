0e2c65755765a9c40047dd83ed8021e529c30551
AlirezaHosseini <ar.hosseini.1177@gmail.com>
2024-10-29 15:12:29 +0330

    best solution to q5

--- Array_string/best_time_buy_sell_stock/best_time_buy_sell_stock.py
+++ Array_string/best_time_buy_sell_stock/best_time_buy_sell_stock.py
@@ -1,21 +1,24 @@
-class Solution(object):
+class Solution:
     def maxProfit(self, prices):
         """
         :type prices: List[int]
         :rtype: int
         """
-        max_dis = 0
-        for i in range(len(prices)):
-            for j in range(i+1,len(prices)):
-                if prices[j] > prices[i]:
-                    dis = prices[j] - prices[i]
-                    if dis > max_dis:
-                        max_dis = dis
 
-        return max_dis
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+
+        return max_profit
+
 
 solution = Solution()
 
-prices = [2,4,1]
+prices = [2, 4, 1]
 
 print(solution.maxProfit(prices))
