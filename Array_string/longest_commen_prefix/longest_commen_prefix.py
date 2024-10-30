42bcc68d3b1b3800510500b3061a1da742376054
AlirezaHosseini <ar.hosseini.1177@gmail.com>
2024-10-30 12:24:56 +0330

    Answer Q8 better

--- Array_string/longest_commen_prefix/longest_commen_prefix.py
+++ Array_string/longest_commen_prefix/longest_commen_prefix.py
@@ -1,27 +1,54 @@
-class Solution(object):
+class Solution:
     def longestCommonPrefix(self, strs):
         """
+        Finds the longest common prefix string amongst an array of strings.
+
         :type strs: List[str]
         :rtype: str
         """
-        index_min = 0
-        for i,words in enumerate(strs):
-            if len(words) < len(strs[index_min]):
-                index_min = i
+        if not strs:
+            return ""
 
-        count = 0
-        counter = ""
-        for i in range(len(strs[index_min])):
-            for j in range(len(strs)):
-                if j != index_min:
-                    if strs[index_min][i] == strs[j][i]:
-                        count += 1
-            if count == len(strs) - 1:
-                counter += strs[index_min][i]
-                count = 0
-            else:
-                return counter
-        return counter
+        # Start with the first string as the initial prefix
+        prefix = strs[0]
+
+        # Iterate through the remaining strings
+        for s in strs[1:]:
+            # Compare the current prefix with the string s
+            while not s.startswith(prefix):
+                # Reduce the prefix by one character
+                prefix = prefix[:-1]
+                # If there's no common prefix
+                if not prefix:
+                    return ""
+
+        return prefix
+
+
+# class Solution(object):
+#     def longestCommonPrefix(self, strs):
+#         """
+#         :type strs: List[str]
+#         :rtype: str
+#         """
+#         index_min = 0
+#         for i,words in enumerate(strs):
+#             if len(words) < len(strs[index_min]):
+#                 index_min = i
+#
+#         count = 0
+#         counter = ""
+#         for i in range(len(strs[index_min])):
+#             for j in range(len(strs)):
+#                 if j != index_min:
+#                     if strs[index_min][i] == strs[j][i]:
+#                         count += 1
+#             if count == len(strs) - 1:
+#                 counter += strs[index_min][i]
+#                 count = 0
+#             else:
+#                 return counter
+#         return counter
 
 
 solution = Solution()
