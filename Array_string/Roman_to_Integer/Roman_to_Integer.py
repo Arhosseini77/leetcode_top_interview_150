b88dd2627435d7b73f1565c268e0f639ca35e766
AlirezaHosseini <ar.hosseini.1177@gmail.com>
2024-10-30 10:12:19 +0330

    clean code q6

--- Array_string/Roman_to_Integer/Roman_to_Integer.py
+++ Array_string/Roman_to_Integer/Roman_to_Integer.py
@@ -4,11 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dic = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         final_int = 0
         for index, roman in enumerate(s):
             value = dic.get(roman)
-            if index == len(s) - 1 or dic.get(s[index + 1]) <=  value:
+            if index == len(s) - 1 or dic.get(s[index + 1]) <= value:
                 final_int += value
             else:
                 final_int -= value
