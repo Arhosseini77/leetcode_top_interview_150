bd332c444380df3024c33b901367d6d5ea83f8a2
AlirezaHosseini <ar.hosseini.1177@gmail.com>
2024-10-30 10:08:42 +0330

    Answer Q6

---
+++ Array_string/Roman_to_Integer/Roman_to_Integer.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        final_int = 0
+        for index, roman in enumerate(s):
+            value = dic.get(roman)
+            if index == len(s) - 1 or dic.get(s[index + 1]) <=  value:
+                final_int += value
+            else:
+                final_int -= value
+        return final_int
+
+
+solution = Solution()
+
+s = "LVIII"
+print(solution.romanToInt(s))
