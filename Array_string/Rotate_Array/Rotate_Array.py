8dc01921692b79393d29b819d717c23e2b5f5739
AlirezaHosseini <ar.hosseini.1177@gmail.com>
2024-11-03 12:34:15 +0330

    better answer for rotate array

--- Array_string/Rotate_Array/Rotate_Array.py
+++ Array_string/Rotate_Array/Rotate_Array.py
@@ -5,8 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        for i in range(k):
-            nums.insert(0, nums.pop(-1))
+        n = len(nums)
+        k = k % n  # handle cases where k > n
+
+        # Reverse the entire array
+        nums.reverse()
+        # Reverse the first k elements
+        nums[:k] = reversed(nums[:k])
+        # Reverse the rest of the elements
+        nums[k:] = reversed(nums[k:])
 
 
 solution = Solution()
