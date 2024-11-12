d630348305958730e08888225ad34c14b49183bb
AlirezaHosseini <ar.hosseini.1177@gmail.com>
2024-11-12 16:57:41 +0330

    Answer dublicated from sort array added

--- Array_string/Remove_Duplicates_from_Sorted_Array_II/Remove_Duplicates_from_Sorted_Array_II.py
+++ Array_string/Remove_Duplicates_from_Sorted_Array_II/Remove_Duplicates_from_Sorted_Array_II.py
@@ -4,3 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        counter = 0
+        temp = nums[0]
+        count = -1
+        for index, value in enumerate(nums):
+            if value == temp and count <1:
+                nums[counter] = nums[index]
+                counter += 1
+                count +=1
+            elif value != temp:
+                nums[counter] = nums[index]
+                counter += 1
+                temp = value
+                count = 0
+
+        print(nums[:counter])
+        return counter
+
+solution = Solution()
+
+nums = [0,0,1,1,1,1,2,3,3]
+print(solution.removeDuplicates(nums))
