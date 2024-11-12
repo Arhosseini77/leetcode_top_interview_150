1f8ebfdb30a12e3379d82954f86e953d024ee960
AlirezaHosseini <ar.hosseini.1177@gmail.com>
2024-11-12 16:26:52 +0330

    Answer sell but stock 1,2 Added

--- Array_string/Best_Time_to_Buy_and_Sell_Stock_I/Best_Time_to_Buy_and_Sell_Stock_I.py
+++ Array_string/Best_Time_to_Buy_and_Sell_Stock_I/Best_Time_to_Buy_and_Sell_Stock_I.py
@@ -4,21 +4,23 @@ class Solution:
         :type prices: List[int]
         :rtype: int
         """
-
         min_price = prices[0]
         max_profit = 0
-
+        max_prices = []
         for price in prices:
             if price < min_price:
                 min_price = price
-            elif price - min_price > max_profit:
+            if price - min_price > max_profit:
                 max_profit = price - min_price
+                max_prices.append(max_profit)
+                max_profit = 0
+                min_price = price
 
-        return max_profit
+        return sum(max_prices)
 
 
 solution = Solution()
 
-prices = [2, 4, 1]
+prices = [1,2,3,4,5]
 
 print(solution.maxProfit(prices))
--- Array_string/Best_Time_to_Buy_and_Sell_Stock_II/Best_Time_to_Buy_and_Sell_Stock_II.py
+++ Array_string/Best_Time_to_Buy_and_Sell_Stock_II/Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -1,6 +1,24 @@
-class Solution(object):
+class Solution:
     def maxProfit(self, prices):
         """
         :type prices: List[int]
         :rtype: int
         """
+
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+
+        return max_profit
+
+
+solution = Solution()
+
+prices = [2, 4, 1]
+
+print(solution.maxProfit(prices))
