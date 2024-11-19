74e4099a1d44144e21c141d65bb421dd5b835ca3
AlirezaHosseini <ar.hosseini.1177@gmail.com>
2024-11-19 17:23:31 +0330

    first versiuon code jump game added but memory limit in test

--- Array_string/Jump_Game/Jump_game.py
+++ Array_string/Jump_Game/Jump_game.py
@@ -1,14 +1,37 @@
+def match_index(index, nums):
+    match_idx = []
+    for i in range(1, nums[index] + 1):
+        if index + i < len(nums):
+            match_idx.append(index + i)
+    return match_idx
+
+
 class Solution(object):
     def canJump(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
+        target = len(nums) - 1
+        if target == 0 and nums:
+            return True
+
+        match_idx = match_index(0, nums)
+
+        while match_idx:
+            if target in match_idx:
+                return True
+            else:
+                match_idx_tmp = []
+                for elm in match_idx:
+                    match_idx_tmp += (match_index(elm, nums))
+            match_idx = list(set(match_idx_tmp))
+        return False
 
 
 # Create an instance of Solution
 solution = Solution()
 
 # Example test
-nums = [2,3,1,1,4]
+nums = [3,2,1,0,4]
 print(solution.canJump(nums))
