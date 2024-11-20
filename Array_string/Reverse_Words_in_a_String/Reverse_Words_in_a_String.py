f8a9fdca2ad413bc879c8dde3cd2f75ca77491c3
AlirezaHosseini <ar.hosseini.1177@gmail.com>
2024-11-20 11:07:47 +0330

    version 1 asnwer reverse words

--- Array_string/Reverse_Words_in_a_String/Reverse_Words_in_a_String.py
+++ Array_string/Reverse_Words_in_a_String/Reverse_Words_in_a_String.py
@@ -4,3 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        words = s.split()
+        words.reverse()
+        s = ""
+        for word in words:
+            s += word
+            if word is not words[-1]:
+                s += " "
+        return s
+
+
+solution = Solution()
+s = "the sky is blue"
+print(solution.reverseWords(s))
