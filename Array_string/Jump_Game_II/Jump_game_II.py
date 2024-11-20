101040314d803fc70f904ba4b408a5d9a8af0413
AlirezaHosseini <ar.hosseini.1177@gmail.com>
2024-11-20 10:29:51 +0330

    Question jump 2 added

---
+++ Array_string/Jump_Game_II/Jump_game_II.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+
+# Create an instance of Solution
+solution = Solution()
+
+# Example test
+nums = [2, 5, 0, 0, 4]
+print(solution.jump(nums))
